4b6ce3cccba7c731ae79705d4804bd1146ecddc6
Natan <ns5933988@gmail.com>
2023-11-25 00:58:06 -0300

    add: adicionado um novo programa em exercicios

--- repl.it/exercicios/alistamento_militar.py
+++ repl.it/exercicios/alistamento_militar.py
@@ -19,9 +19,34 @@ if idade < 18:
     
     print("Você tem {} anos em {} e vai se alistar daqui a {} anos, em {}.".format(
         idade, ano_atual, (18 - idade), (nasc + 18)))
-elif idade > 18:
+elif idade > 18:    
+    
     print("Você tem {} anos e já passou seu período de alistamento.".format(idade))
     print("Se não se alistou, deveria ter se alistado em {}, há {} anos atrás.".format(
         (nasc + 18), idade - 18))
 else:
     print("Você tem {} anos. Está na hora de se alistar.".format(idade))
+
+
+# caso fosse em modo função,
+# estaria também dessa forma 
+'''
+def ray():
+    from datetime import datetime
+    nasc = int(input("nascimento: "))
+    ano_atual = datetime.now().year
+    idade = (ano_atual - nasc)
+
+    if idade > 18:
+
+        print("você tem idade suficiente para tal")
+    elif idade < 18:
+        print("me parece que a sua idade está abaixo quanto" +
+              + " aos padrões que assim foram especificados")
+
+    else:
+        print("idade fora dos conformes estabelecidos")
+
+ray()
+
+''' 
